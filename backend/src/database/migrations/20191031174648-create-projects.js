d452c0506a7e76093d440e5acc98b999b10b1d06
José Rafael <zerafael1@gmail.com>
2019-10-31 13:51:30 -0400

    Add projects migration

---
+++ backend/src/database/migrations/20191031174648-create-projects.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('projects', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      data_inicio: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      data_fim: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('projects');
+  },
+};
