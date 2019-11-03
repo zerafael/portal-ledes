7b830510956974675b105ad3ac9777f4eac719e8
José Rafael <zerafael1@gmail.com>
2019-11-03 16:37:02 -0400

    Add new models to database

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,10 +1,34 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import News from '../app/models/News';
+import Attachment from '../app/models/Attachment';
+import ImageNews from '../app/models/ImageNews';
+import Member from '../app/models/Member';
+import MemberType from '../app/models/MemberType';
+import Role from '../app/models/Role';
+import MemberRole from '../app/models/MemberRole';
+import Project from '../app/models/Project';
+import Team from '../app/models/Team';
+import History from '../app/models/History';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [
+  User,
+  File,
+  News,
+  Attachment,
+  ImageNews,
+  Member,
+  MemberType,
+  Role,
+  MemberRole,
+  Project,
+  Team,
+  History,
+];
 
 class Database {
   constructor() {
@@ -14,7 +38,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
