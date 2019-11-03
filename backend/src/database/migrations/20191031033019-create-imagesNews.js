5bd5d49574459cba1ebd0f72ab106a79bb372be1
José Rafael <zerafael1@gmail.com>
2019-11-03 16:36:29 -0400

    Add id column on attachment and imagesNews migrations

--- backend/src/database/migrations/20191031033019-create-imagesNews.js
+++ backend/src/database/migrations/20191031033019-create-imagesNews.js
@@ -1,6 +1,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('imagesNews', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
       news: {
         type: Sequelize.INTEGER,
         references: { model: 'news', key: 'id' },
