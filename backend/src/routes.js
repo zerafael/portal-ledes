eb8486d51450951471e8425d3238cb0908a5a885
José Rafael <zerafael1@gmail.com>
2019-11-03 23:32:52 -0400

    Add new routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,39 @@
 import { Router } from 'express';
 
+import LoginController from './app/controllers/LoginController';
+import UserController from './app/controllers/UserController';
+import NewsController from './app/controllers/NewsController';
+import ProjectController from './app/controllers/ProjectController';
+import RoleController from './app/controllers/RoleController';
+
+import authMiddleware from './app/middlewares/auth';
+import adminMiddleware from './app/middlewares/admin';
+
 const routes = new Router();
 
 routes.get('/', (request, response) => {
   return response.send('Hello!');
 });
+
+// Login
+routes.post('/login', LoginController.store);
+
+routes.use(authMiddleware);
+
+// Create user
+routes.post('/users', adminMiddleware, UserController.store);
+// Update user
+routes.put('/users', UserController.update);
+
+// Create news
+routes.post('/news', NewsController.store);
+// List news
+routes.get('/news', NewsController.index);
+
+// Create project
+routes.post('/projects', adminMiddleware, ProjectController.store);
+
+// Create role
+routes.post('/roles', adminMiddleware, RoleController.store);
 
 export default routes;
