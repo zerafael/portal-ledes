cd81a9df1a77c3fd3f80d352051a969ecac147a3
José Rafael <zerafael1@gmail.com>
2019-11-06 23:42:12 -0400

    Update routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ import UserController from './app/controllers/UserController';
 import NewsController from './app/controllers/NewsController';
 import ProjectController from './app/controllers/ProjectController';
 import RoleController from './app/controllers/RoleController';
+import MemberController from './app/controllers/MemberController';
+import MemberProjectController from './app/controllers/MemberProjectController';
 
 import authMiddleware from './app/middlewares/auth';
 import adminMiddleware from './app/middlewares/admin';
@@ -17,6 +19,9 @@ routes.get('/', (request, response) => {
 
 // Login
 routes.post('/login', LoginController.store);
+
+// List projects
+routes.get('/projects', ProjectController.index);
 
 routes.use(authMiddleware);
 
@@ -32,8 +37,31 @@ routes.get('/news', NewsController.index);
 
 // Create project
 routes.post('/projects', adminMiddleware, ProjectController.store);
+// Update project
+routes.put('/projects/:id', adminMiddleware, ProjectController.update);
+// Delete project
+routes.delete('/projects/:id', adminMiddleware, ProjectController.delete);
 
 // Create role
 routes.post('/roles', adminMiddleware, RoleController.store);
+routes.get('/roles', adminMiddleware, RoleController.index);
+
+// Create member
+routes.post('/members', adminMiddleware, MemberController.store);
+// List Members
+routes.get('/members', adminMiddleware, MemberController.index);
+
+// Add members with roles to project
+routes.post(
+  '/project/:id/members',
+  adminMiddleware,
+  MemberProjectController.store
+);
+// List members from a project
+routes.get(
+  '/project/:id/members',
+  adminMiddleware,
+  MemberProjectController.index
+);
 
 export default routes;
